483b9f12593ca5f8fda73e74e16182c525690447
aagr29 <63660748+aagr29@users.noreply.github.com>
2022-07-06 12:13:15 +0930

    ceo dashboard

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/neovis.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(process.env.PORT || port, () => {
